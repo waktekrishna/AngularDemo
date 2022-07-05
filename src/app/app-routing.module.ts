0cecebb7315a98c85ac346b048208e276c48c650
akshaywakte <akshaywakte@gmail.com>
2022-07-05 14:50:28 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BatchesComponent } from './batches/batches.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InvalidComponent } from './invalid/invalid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path : 'batches' , component : BatchesComponent},
+{ path : 'Books' , component : SubjectsComponent},
+{ path : 'Technologies' , component : AboutUsComponent},
+{ path : '' , component : AboutUsComponent},
+{ path : '**' , component : InvalidComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule 
+{
+  
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BatchesComponent } from './batches/batches.component';
+import { SubjectsComponent } from './subjects/subjects.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InvalidComponent } from './invalid/invalid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BatchesComponent,
+    SubjectsComponent,
+    AboutUsComponent,
+    InvalidComponent
   ],
   imports: [
     BrowserModule,
